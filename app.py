bcb2ca85052a0d5855d5c55d16d556d1ba50c9c1
Junghyerim <thsehddnr68@gmail.com>
2025-11-13 09:51:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 MV_LIST = ['Ct8NZdYWOFI','g3TP6XZ1Baw','0ZukHxqOA0o','slT80EySpKk','BYQBs_4-MOo','7WINyXmPRAE','GHu39FEFIks','cxcxskPKtiI','Rh5ok0ljrzA','f_iQRO5BdCM','ouR4nn1G9r4','qGWZUtfV3IU','BkLKEsh6tZU','EiVmQZwJhsA','jeqdYqsrsA0','npttud7NkL0','js3PTlQFakk','BzYnNdJhZQw','86BST8NIpNM','d9IxdwEFk1c','R3Fwdnij49o','TgOu00Mf3kI','NJR8Inf77Ac','42Gtm4-Ax2U','v7bnOxV4jAc','nM0xDI5R50E','sqgxcCjD04s','D1PvIWdJ8xo','0-q1KafFCLU','Hsuy_xzPyWQ','VIDQTyNmkN4','c9E2IT1jHQY','l5Z1PBJLUss','ZXmoJu81e6A','6J9ixwhDYSM','mFbILexYSQg','JleoAppaxi0','kHW-UVXOcLU']
 
 # 라이브클립 ID 리스트
-LIVE_LIST = ['K2nL1o2G79E','JtFI8dtPvxI','8zsYZFvKniw','m7mvpe1fVa4','L1JUfCyeT5E','3mk-DIcvVGU','3nDzKulmSpg','r3WS1BOpgk4','j3Aa1dgg8UI','FAI2wj2JGCI','UCQHgJ4uRwo','SmQhRHS9-YA','mcnFWBLrdCs','Xco5vbBmF5c','sAbU4fAqjZk','O-1FpjPP6_c','-_14Lhw0y1A','9WKzt9QEmD4','pDvBiB1waBk','nn1pbxe8bAI','o_nxIQTM_B0','3iM_06QeZi8','tJM0yIbg8iQ','OcVmaIlHZ1o','ax1csKKQnns','Cxzzg7L3Xgc']
+LIVE_LIST = ['JtFI8dtPvxI','8zsYZFvKniw','m7mvpe1fVa4','L1JUfCyeT5E','3mk-DIcvVGU','3nDzKulmSpg','r3WS1BOpgk4','j3Aa1dgg8UI','FAI2wj2JGCI','UCQHgJ4uRwo','SmQhRHS9-YA','mcnFWBLrdCs','Xco5vbBmF5c','sAbU4fAqjZk','O-1FpjPP6_c','-_14Lhw0y1A','9WKzt9QEmD4','pDvBiB1waBk','nn1pbxe8bAI','o_nxIQTM_B0','3iM_06QeZi8','tJM0yIbg8iQ','OcVmaIlHZ1o','ax1csKKQnns','Cxzzg7L3Xgc']
 
 def get_next_update_time():
     """다음 10분 단위 업데이트 시간 계산 (KST 기준)"""
@@ -140,4 +140,5 @@ def update_data():
 
 if __name__ == '__main__':
     app.run(port = 80, debug=True, host = '0.0.0.0')
+
 
